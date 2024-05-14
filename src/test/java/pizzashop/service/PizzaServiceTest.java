a4428e559081681745d9548c6e424363c7b19d55
razvanclaudiu <tomutarazvan21@gmail.com>
2024-05-14 18:16:58 +0300

    Added white box tests to PizzaServiceTest

--- src/test/java/pizzashop/service/PizzaServiceTest.java
+++ src/test/java/pizzashop/service/PizzaServiceTest.java
@@ -1,11 +1,17 @@
 package pizzashop.service;
 
 import org.junit.jupiter.api.*;
+import pizzashop.model.Payment;
 import pizzashop.model.PaymentType;
 import pizzashop.repository.MenuRepository;
 import pizzashop.repository.PaymentRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PizzaServiceTest {
@@ -153,6 +159,40 @@ class PizzaServiceTest {
         //assert
         assertEquals(expectedMessage, exception.getMessage());
         assertEquals(noOfElements, pizzaService.getPayments().size());
+    }
+
+    @Test
+    @Order(8)
+    void getTotalAmount_F02_TC03() {
+        MenuRepository menuRepository = new MenuRepository();
+        PaymentRepository paymentRepository = mock(PaymentRepository.class);
+        List<Payment> payments = new ArrayList<>();
+        payments.add(new Payment(1, PaymentType.CASH, 23.0));
+        payments.add(new Payment(2, PaymentType.CASH, 54.0));
+        payments.add(new Payment(3, PaymentType.CASH, 65.0));
+        when(paymentRepository.getAll()).thenReturn(payments);
+        PizzaService pizzaService = new PizzaService(menuRepository, paymentRepository);
+
+        double result = pizzaService.getTotalAmount(PaymentType.CASH);
+
+        assertEquals(172.0, result);
+    }
+
+    @Test
+    @Order(9)
+    void getTotalAmount_F02_TC04() {
+        MenuRepository menuRepository = new MenuRepository();
+        PaymentRepository paymentRepository = mock(PaymentRepository.class);
+        List<Payment> payments = new ArrayList<>();
+        payments.add(new Payment(1, PaymentType.CASH, 23.0));
+        payments.add(new Payment(2, PaymentType.CASH, 54.0));
+        payments.add(new Payment(3, PaymentType.CASH, 65.0));
+        when(paymentRepository.getAll()).thenReturn(payments);
+        PizzaService pizzaService = new PizzaService(menuRepository, paymentRepository);
+
+        double result = pizzaService.getTotalAmount(PaymentType.CARD);
+
+        assertEquals(0.0f, result);
     }
 
     @Test
